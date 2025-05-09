04ba03594ff73a7a04aa510f725aa352b733df44
“ffeller” <flaris.feller@gmail.com>
2025-05-09 20:35:52 -0300

    Instalando ESLint workflow

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var test = 0; 
+
 app.get('/', (req, res) => {
     res.sendFile('views/index.html', { root: __dirname })
 })
