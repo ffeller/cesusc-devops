d02c45b5ae8a5236334253acbdd15bb394885fe4
“ffeller” <flaris.feller@gmail.com>
2025-05-09 20:58:14 -0300

    Revisão do ESLint

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var test = 0; 
+// var test = 0; 
 
 app.get('/', (req, res) => {
+    // eslint-disable-next-line no-undef
     res.sendFile('views/index.html', { root: __dirname })
 })
 
